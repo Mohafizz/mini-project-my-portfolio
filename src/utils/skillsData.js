ffeb0959776859e1d8bd540323f41e4b5a846794
mohafizz <mohafizz@gmail.com>
2018-03-27 17:14:58 +0800

    safe return point for old and new skills

--- src/utils/skillsData.js
+++ src/utils/skillsData.js
@@ -1,17 +1,17 @@
 export const oldSkillsData = [
-  { subject: "A", A: 10, fullMark: 10 },
-  { subject: "B", A: 9, fullMark: 10 },
-  { subject: "C", A: 8, fullMark: 10 },
-  { subject: "D", A: 9, fullMark: 10 },
-  { subject: "E", A: 8, fullMark: 10 },
-  { subject: "F", A: 6, fullMark: 10 }
+  { subject: "Electronics", A: 7, fullMark: 10 },
+  { subject: "Troubleshooting", A: 7.5, fullMark: 10 },
+  { subject: "Soldering", A: 8, fullMark: 10 },
+  { subject: "LabView", A: 7, fullMark: 10 },
+  { subject: "Team Management", A: 7.5, fullMark: 10 },
+  { subject: "Lab management", A: 7, fullMark: 10 }
 ];
 
 export const newSkillsData = [
-  { subject: "A", B: 10, fullMark: 10 },
-  { subject: "B", B: 9, fullMark: 10 },
-  { subject: "C", B: 8, fullMark: 10 },
-  { subject: "D", B: 9, fullMark: 10 },
-  { subject: "E", B: 8, fullMark: 10 },
-  { subject: "F", B: 6, fullMark: 10 }
+  { subject: "Test Driven Development", B: 6, fullMark: 10 },
+  { subject: "Javascript", B: 6, fullMark: 10 },
+  { subject: "Agile Approach", B: 8, fullMark: 10 },
+  { subject: "Project Management", B: 7, fullMark: 10 },
+  { subject: "Problem Solving", B: 8, fullMark: 10 },
+  { subject: "Troubleshooting", B: 6, fullMark: 10 }
 ];
