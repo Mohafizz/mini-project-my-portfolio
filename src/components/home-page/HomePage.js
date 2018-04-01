3663612cdc795c76f0499e03b8534f60d5f70bc7
mohafizz <mohafizz@gmail.com>
2018-04-01 18:52:17 +0800

    safe point to upload to heroku

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,5 +1,36 @@
 import React from "react";
+import "./HomePage.css";
+import { Grid, Cell } from "react-mdl";
 
-const HomePage = () => <div>Homepage</div>;
+const HomePage = () => {
+  return (
+    <div>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="projects-grid">
+          <Cell col={4} tablet={6} phone={3}>
+            <img
+              className="homepage-avatar"
+              src="Images/mohafizz_avatar.jpg"
+              alt="mohafizz"
+            />
+          </Cell>
+
+          <Cell col={6} tablet={8}>
+            <div className="homepage-text">
+              <h3>Hello there!</h3>
+              <p>
+                My name is Mohamed Hafiz Bin Abu Bakar. This is my journey as a
+                Jumpstarter to switch my career into Tech. Please feel free to
+                browse to this website to view all the projects that I am still
+                embarking.
+              </p>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+      <br />
+    </div>
+  );
+};
 
 export default HomePage;
